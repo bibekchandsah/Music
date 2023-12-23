457790b97f20583dd2b7ef7c755390568455923c
bibek10550 <84659279+bibek10550@users.noreply.github.com>
2023-12-24 00:09:19 +0530

    Add files via upload

---
+++ js/2music.js
@@ -0,0 +1,420 @@
+const navbarr = document.querySelectorAll(".nav");
+const wrapppers = document.querySelectorAll(".wrappper");
+const musics = document.querySelectorAll(".wrapperr");
+const navv = document.querySelectorAll(".nav div");
+const wrapppper = document.querySelectorAll(".wrappper div");
+const music = document.querySelectorAll(".wrapperr .loads");
+const renderCard = () => {
+  //Remove the skeleton loading effect
+  navbarr.forEach((navbar) => {
+    navbar.classList.remove("loading");
+  });
+  wrapppers.forEach((onlinechecker) => {
+    onlinechecker.classList.remove("loading");
+  });
+  musics.forEach((musics) => {
+    musics.classList.remove("loading");
+  });
+  //Show the hidden html elements
+  navv.forEach((navbarDiv) => {
+    navbarDiv.style.visibility = "visible";
+  });
+  wrapppper.forEach((wrappperDiv) => {
+    wrappperDiv.style.visibility = "visible";
+  });
+  music.forEach((musicDiv) => {
+    musicDiv.style.visibility = "visible";
+  });
+}
+// Execute renderCard on setTimeout
+// setTimeout(() => {
+//   renderCard();
+// }, 4000);
+//Execute renderCard after the page loaded
+window.addEventListener("load", () => {
+  renderCard();
+});
+
+// music function 
+const wrapper = document.querySelector(".wrapperr"),
+  musicImg = wrapper.querySelector(".img-area img"),
+  musicName = wrapper.querySelector(".song-details .name"),
+  musicArtist = wrapper.querySelector(".song-details .artist"),
+  playPauseBtn = wrapper.querySelector(".play-pause"),
+  prevBtn = wrapper.querySelector("#prev"),
+  nextBtn = wrapper.querySelector("#next"),
+  mainAudio = wrapper.querySelector("#main-audio"),
+  progressArea = wrapper.querySelector(".progress-area"),
+  progressBar = progressArea.querySelector(".progress-bar"),
+  musicList = wrapper.querySelector(".music-list"),
+  moreMusicBtn = wrapper.querySelector("#more-music"),
+  closemoreMusic = musicList.querySelector("#closee");
+
+let musicIndex = Math.floor((Math.random() * allMusic.length) + 1);
+isMusicPaused = true;
+
+window.addEventListener("load", () => {
+  loadMusic(musicIndex);
+  playingSong();
+});
+
+
+
+// Function to initialize the music player
+function initializeMusicPlayer() {
+  // Declare ulTag within the function
+  const ulTag = wrapper.querySelector("ul");
+
+  // Your existing code for loading music and rendering cards
+
+  // Event listeners for play, pause, next, and previous buttons
+  // playPauseBtn.addEventListener("click", togglePlayPause);
+  prevBtn.addEventListener("click", prevMusic);
+  nextBtn.addEventListener("click", nextMusic);
+
+  // Event listener for updating the progress bar
+  // progressArea.addEventListener("click", updateProgressBar);
+
+  // Event listener for changing loop, shuffle, repeat icon onclick
+  // repeatBtn.addEventListener("click", changeRepeatMode);
+
+  // Event listener for when the song ends
+  // mainAudio.addEventListener("ended", handleSongClick);
+
+  // Event listener for showing/hiding the music list
+  // moreMusicBtn.addEventListener("click", toggleMusicList);
+  // closemoreMusic.addEventListener("click", toggleMusicList);
+
+  // Event listeners for clicking on a song in the list
+  const allLiTag = ulTag.querySelectorAll("li");
+  allLiTag.forEach((li) => {
+    li.addEventListener("click", () => handleSongClick(li));
+  });
+}
+
+
+
+
+
+
+
+
+// function loadMusic(indexNumb){
+//   musicName.innerText = allMusic[indexNumb - 1].name;
+//   musicArtist.innerText = allMusic[indexNumb - 1].artist;
+//   musicImg.src = `images/${allMusic[indexNumb - 1].src}.jpg`;
+//   mainAudio.src = `songs/${allMusic[indexNumb - 1].src}.mp3`;
+// }
+
+// Function to load music using URLs
+function loadMusic(indexNumb) {
+  musicName.innerText = allMusic[indexNumb - 1].name;
+  musicArtist.innerText = allMusic[indexNumb - 1].artist;
+  musicImg.src = allMusic[indexNumb - 1].img;
+  mainAudio.src = allMusic[indexNumb - 1].src;
+}
+
+// Function to handle a song click in the list
+// function handleSongClick(li) {
+//   const liIndex = li.getAttribute("li-index");
+//   musicIndex = liIndex;
+//   loadMusic(musicIndex);
+//   playMusic();
+//   playingSong();
+// }
+function handleSongClick(li) {
+  const liIndex = li.getAttribute("li-index");
+  musicIndex = liIndex;
+  loadMusic(musicIndex);
+  playMusic();
+  playingSong();
+}
+
+
+
+
+//play music function
+function playMusic() {
+  wrapper.classList.add("paused");
+  playPauseBtn.querySelector("i").innerText = "pause";
+  mainAudio.play();
+}
+
+//pause music function
+function pauseMusic() {
+  wrapper.classList.remove("paused");
+  playPauseBtn.querySelector("i").innerText = "play_arrow";
+  mainAudio.pause();
+}
+
+//prev music function
+function prevMusic() {
+  musicIndex--; //decrement of musicIndex by 1
+  //if musicIndex is less than 1 then musicIndex will be the array length so the last music play
+  musicIndex < 1 ? musicIndex = allMusic.length : musicIndex = musicIndex;
+  loadMusic(musicIndex);
+  playMusic();
+  playingSong();
+}
+
+//next music function
+function nextMusic() {
+  musicIndex++; //increment of musicIndex by 1
+  //if musicIndex is greater than array length then musicIndex will be 1 so the first music play
+  musicIndex > allMusic.length ? musicIndex = 1 : musicIndex = musicIndex;
+  loadMusic(musicIndex);
+  playMusic();
+  playingSong();
+}
+
+// play or pause button event// Function to toggle play/pause
+playPauseBtn.addEventListener("click", () => {
+  const isMusicPlay = wrapper.classList.contains("paused");
+  //if isPlayMusic is true then call pauseMusic else call playMusic
+  isMusicPlay ? pauseMusic() : playMusic();
+  playingSong();
+});
+
+
+//prev music button event
+prevBtn.addEventListener("click", () => {
+  prevMusic();
+});
+
+//next music button event
+nextBtn.addEventListener("click", () => {
+  nextMusic();
+});
+
+// update progress bar width according to music current time
+mainAudio.addEventListener("timeupdate", (e) => {
+  const currentTime = e.target.currentTime; //getting playing song currentTime
+  const duration = e.target.duration; //getting playing song total duration
+  let progressWidth = (currentTime / duration) * 100;
+  progressBar.style.width = `${progressWidth}%`;
+
+  let musicCurrentTime = wrapper.querySelector(".current-time"),
+    musicDuartion = wrapper.querySelector(".max-duration");
+  mainAudio.addEventListener("loadeddata", () => {
+    // update song total duration
+    let mainAdDuration = mainAudio.duration;
+    let totalMin = Math.floor(mainAdDuration / 60);
+    let totalSec = Math.floor(mainAdDuration % 60);
+    if (totalSec < 10) { //if sec is less than 10 then add 0 before it
+      totalSec = `0${totalSec}`;
+    }
+    musicDuartion.innerText = `${totalMin}:${totalSec}`;
+  });
+  // update playing song current time
+  let currentMin = Math.floor(currentTime / 60);
+  let currentSec = Math.floor(currentTime % 60);
+  if (currentSec < 10) { //if sec is less than 10 then add 0 before it
+    currentSec = `0${currentSec}`;
+  }
+  musicCurrentTime.innerText = `${currentMin}:${currentSec}`;
+});
+
+// update playing song currentTime on according to the progress bar width
+progressArea.addEventListener("click", (e) => {
+  let progressWidth = progressArea.clientWidth; //getting width of progress bar
+  let clickedOffsetX = e.offsetX; //getting offset x value
+  let songDuration = mainAudio.duration; //getting song total duration
+
+  mainAudio.currentTime = (clickedOffsetX / progressWidth) * songDuration;
+  playMusic(); //calling playMusic function
+  playingSong();
+});
+
+//change loop, shuffle, repeat icon onclick
+const repeatBtn = wrapper.querySelector("#repeat-plist");
+repeatBtn.addEventListener("click", () => {
+  let getText = repeatBtn.innerText; //getting this tag innerText
+  switch (getText) {
+    case "repeat":
+      repeatBtn.innerText = "repeat_one";
+      repeatBtn.setAttribute("title", "Song looped");
+      break;
+    case "repeat_one":
+      repeatBtn.innerText = "shuffle";
+      repeatBtn.setAttribute("title", "Playback shuffled");
+      break;
+    case "shuffle":
+      repeatBtn.innerText = "repeat";
+      repeatBtn.setAttribute("title", "Playlist looped");
+      break;
+  }
+});
+
+//code for what to do after song ended
+mainAudio.addEventListener("ended", () => {
+  // we'll do according to the icon means if user has set icon to
+  // loop song then we'll repeat the current song and will do accordingly
+  let getText = repeatBtn.innerText; //getting this tag innerText
+  switch (getText) {
+    case "repeat":
+      nextMusic(); //calling nextMusic function
+      break;
+    case "repeat_one":
+      mainAudio.currentTime = 0; //setting audio current time to 0
+      loadMusic(musicIndex); //calling loadMusic function with argument, in the argument there is a index of current song
+      playMusic(); //calling playMusic function
+      break;
+    case "shuffle":
+      let randIndex = Math.floor((Math.random() * allMusic.length) + 1); //genereting random index/numb with max range of array length
+      do {
+        randIndex = Math.floor((Math.random() * allMusic.length) + 1);
+      } while (musicIndex == randIndex); //this loop run until the next random number won't be the same of current musicIndex
+      musicIndex = randIndex; //passing randomIndex to musicIndex
+      loadMusic(musicIndex);
+      playMusic();
+      playingSong();
+      break;
+  }
+});
+
+//show music list onclick of music icon
+moreMusicBtn.addEventListener("click", () => {
+  musicList.classList.toggle("show");
+});
+closemoreMusic.addEventListener("click", () => {
+  moreMusicBtn.click();
+});
+
+// const ulTag = wrapper.querySelector("ul");
+// // let create li tags according to array length for list
+// for (let i = 0; i < allMusic.length; i++) {
+//   //let's pass the song name, artist from the array
+//   let liTag = `<li li-index="${i + 1}">
+//                 <div class="row">
+//                   <span>${allMusic[i].name}</span>
+//                   <p>${allMusic[i].artist}</p>
+//                 </div>
+//                 <span id="${allMusic[i].src}" class="audio-duration">3:40</span>
+//                 <audio class="${allMusic[i].src}" src="songs/${allMusic[i].src}.mp3"></audio>
+//               </li>`;
+//   ulTag.insertAdjacentHTML("beforeend", liTag); //inserting the li inside ul tag
+
+//   let liAudioDuartionTag = ulTag.querySelector(`#${allMusic[i].src}`);
+//   let liAudioTag = ulTag.querySelector(`.${allMusic[i].src}`);
+//   liAudioTag.addEventListener("loadeddata", ()=>{
+//     let duration = liAudioTag.duration;
+//     let totalMin = Math.floor(duration / 60);
+//     let totalSec = Math.floor(duration % 60);
+//     if(totalSec < 10){ //if sec is less than 10 then add 0 before it
+//       totalSec = `0${totalSec}`;
+//     };
+//     liAudioDuartionTag.innerText = `${totalMin}:${totalSec}`; //passing total duation of song
+//     liAudioDuartionTag.setAttribute("t-duration", `${totalMin}:${totalSec}`); //adding t-duration attribute with total duration value
+//   });
+// }
+
+const ulTag = wrapper.querySelector("ul");
+// Function to render the music list
+function renderMusicList() {
+  const ulTag = wrapper.querySelector("ul");
+
+  // Loop through allMusic array to generate list items
+  for (let i = 0; i < allMusic.length; i++) {
+    // Generate a unique ID for each audio element
+    const audioId = `audio-${i}`;
+
+    // Generate liTag
+    let liTag = `<li li-index="${i + 1}">
+                  <div class="row">
+                    <span>${allMusic[i].name}</span>
+                    <p>${allMusic[i].artist}</p>
+                  </div>
+                  <span class="audio-duration" data-duration="3:40"></span>
+                  <audio id="${audioId}" src="${allMusic[i].src}"></audio>
+                </li>`;
+
+    ulTag.insertAdjacentHTML("beforeend", liTag);
+
+    let liAudioDuartionTag = ulTag.querySelector(`[id="${audioId}"] + .audio-duration`);
+    let liAudioTag = ulTag.querySelector(`#${audioId}`);
+
+    liAudioTag.addEventListener("loadeddata", () => {
+      // Update the duration once the audio is loaded
+      let duration = liAudioTag.duration;
+      let totalMin = Math.floor(duration / 60);
+      let totalSec = Math.floor(duration % 60);
+      if (totalSec < 10) {
+        totalSec = `0${totalSec}`;
+      }
+      liAudioDuartionTag.innerText = `${totalMin}:${totalSec}`;
+      liAudioDuartionTag.setAttribute("t-duration", `${totalMin}:${totalSec}`); //adding t-duration attribute with total duration value
+    });
+  }
+}
+
+//play particular song from the list onclick of li tag
+// function playingSong() {
+//   const allLiTag = ulTag.querySelectorAll("li");
+
+//   for (let j = 0; j < allLiTag.length; j++) {
+//     let audioTag = allLiTag[j].querySelector(".audio-duration");
+
+//     if (allLiTag[j].classList.contains("playing")) {
+//       allLiTag[j].classList.remove("playing");
+//       let adDuration = audioTag.getAttribute("t-duration");
+//       audioTag.innerText = adDuration;
+//     }
+
+//     //if the li tag index is equal to the musicIndex then add playing class in it
+//     if (allLiTag[j].getAttribute("li-index") == musicIndex) {
+//       allLiTag[j].classList.add("playing");
+//       audioTag.innerText = "Playing";
+//     }
+
+//     allLiTag[j].setAttribute("onclick", "clicked(this)");
+//   }
+// }
+
+
+// Function to play a particular song from the list onclick of li tag
+function playingSong(ulTag) {
+  const allLiTag = ulTag.querySelectorAll("li");
+
+  for (let j = 0; j < allLiTag.length; j++) {
+    let audioTag = allLiTag[j].querySelector(".audio-duration");
+
+    if (allLiTag[j].classList.contains("playing")) {
+      allLiTag[j].classList.remove("playing");
+      let adDuration = audioTag.getAttribute("data-duration");
+      audioTag.innerText = adDuration;
+    }
+
+    if (allLiTag[j].getAttribute("li-index") == musicIndex) {
+      allLiTag[j].classList.add("playing");
+      audioTag.innerText = "Playing";
+    }
+
+    allLiTag[j].setAttribute("onclick", "clicked(this)");
+  }
+}
+// Call the playingSong function
+// playingSong(ulTag);
+
+
+
+
+
+//particular li clicked function
+function clicked(element) {
+  let getLiIndex = element.getAttribute("li-index");
+  musicIndex = getLiIndex; //updating current song index with clicked li index
+  loadMusic(musicIndex);
+  playMusic();
+  playingSong(ulTag);
+}
+
+
+
+
+
+// Call the renderMusicList function when the window loads
+window.addEventListener("load", () => {
+  renderMusicList();
+  initializeMusicPlayer();
+  renderCard();
+});
---
+++ js/music-list.js
@@ -0,0 +1,65 @@
+// Modify your allMusic array with URLs
+let allMusic = [
+    {
+      name: "I Guess I m In Love",
+      artist: "Clinton Kane",
+      img: "https://ww-pagalworld.com/uploads/thumb/sft28/13834_4.jpg",
+      src: "https://ww-pagalworld.com/files/download/id/13834"
+    },
+    {
+      name: "My Baby Love",
+      artist: "Jony",
+      img: "https://ww-pagalworld.com/uploads/thumb/sft14/6851_4.jpg",
+      src: "https://ww-pagalworld.com/files/download/id/6851"
+    },
+    {
+      name: "Gasolina",
+      artist: "Daddy Yankee",
+      img: "https://ww-pagalworld.com/uploads/thumb/sft26/12922_4.jpg",
+      src: "https://ww-pagalworld.com/files/download/id/12922"
+    //   src: "https://www.jiosaavn.com/song/gasolina/RAAjZENnT0s"
+    },
+    {
+      name: "Heeriye Heeriye Aa",
+      artist: "Arijit Singh",
+      img: "https://ww-pagalworld.com/uploads/thumb/sft27/13270_4.jpg",
+      src: "https://ww-pagalworld.com/files/download/id/13270"
+    },
+    {
+      name: "Calm Down Slowed Reverb",
+      artist: "Rema",
+      img: "https://ww-pagalworld.com/uploads/thumb/sft26/12678_4.jpg",
+      src: "https://ww-pagalworld.com/files/download/id/12678"
+    },
+    {
+      name: "Taki Taki",
+      artist: "DJ Snake, Selena Gomez",
+      img: "https://pagalworlld.com/siteuploads/thumb/sft11/5088_4.jpg",
+      src: "https://pagalworlld.com/files/download/id/5088"
+    },
+    {
+      name: "Let Me Love You",
+      artist: "Justin Bieber",
+      img: "https://img.wynk.in/unsafe/248x248/filters:no_upscale():strip_exif():format(webp)/http://s3-ap-south-1.amazonaws.com/wynk-music-cms/srch_universalmusic/music/update/1599607201/srch_universalmusic_00602557056273-QMZSY1600019.jpg",
+      src: "https://paglasongs.com/files/download/id/3537"
+    },
+    {
+      name: "Baby",
+      artist: "Justin Bieber",
+      img: "https://pagalworlld.com/siteuploads/thumb/sft10/4702_4.jpg",
+      src: "https://pagalworlld.com/files/download/id/4702"
+    },
+    {
+      name: "Despacito",
+      artist: "Daddy Yankee, Luis Fonsi",
+      img: "https://pagalworld.ink/siteuploads/thumb/sft6/2899_6_resize2x_300x300.webp",
+      src: "https://paglasongs.com/files/download/id/2110"
+    },
+    // {
+    //   name: "",
+    //   artist: "",
+    //   img: "",
+    //   src: ""
+    // },
+    // ... (add more entries with image and music URLs)
+  ];
